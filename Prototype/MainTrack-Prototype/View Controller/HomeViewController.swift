ae336d3bc84426952e7bbf6f1c3d446cbb2b0983
Clay Suttner <csuttner@gmail.com>
2021-05-26 09:16:24 -0700

    Configures Home view controller class for four static rows and removes boilerplate code

---
+++ Prototype/MainTrack-Prototype/View Controller/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  MainTrack-Prototype
+//
+//  Created by Clay Suttner on 5/25/21.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+         self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+}
