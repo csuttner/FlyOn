faf5c8309677e22843991a135bd740638169feab
Clay Suttner <csuttner@gmail.com>
2021-05-26 09:22:27 -0700

    Configures home screen to hide back button and show navigation bar on view will appear

--- Prototype/MainTrack-Prototype/View Controller/HomeViewController.swift
+++ Prototype/MainTrack-Prototype/View Controller/HomeViewController.swift
@@ -11,14 +11,17 @@ class HomeViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-         self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureNavigationController()
     }
 
+    private func configureNavigationController() {
+        navigationItem.hidesBackButton = true
+        navigationController?.navigationBar.isHidden = false
+    }
+    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
