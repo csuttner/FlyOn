8def800a1bb59a45cd71f71ff57b39fea6d990bb
Clay Suttner <csuttner@gmail.com>
2021-06-03 07:28:53 -0700

    Configures navigation item with cancel button, search controller, and title view

--- Prototype/MainTrack-Prototype/View Controller/NewDefectViewController.swift
+++ Prototype/MainTrack-Prototype/View Controller/NewDefectViewController.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class NewDefectViewController: UITableViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let searchController = UISearchController()
+        navigationItem.searchController = searchController
+        
+        let cancelButton = UIButton()
+        cancelButton.setTitle("cancel", for: .normal)
+        
+        let cancelButtonItem = UIBarButtonItem(customView: cancelButton)
+        navigationItem.leftBarButtonItem = cancelButtonItem
+        
+        let subheadingLabel = UILabel()
+        subheadingLabel.text = "Choose an aircraft"
+        subheadingLabel.textColor = .systemGray
+        
+        navigationItem.titleView = subheadingLabel
+    }
 }
