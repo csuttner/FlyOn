524908562f08a058bf02a10a13fe9d94066dad6e
Clay Suttner <csuttner@gmail.com>
2021-06-03 07:14:16 -0700

    Adds New Defect view controller class and storyboard representation

---
+++ Prototype/MainTrack-Prototype/View Controller/NewDefectViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewDefectViewController.swift
+//  MainTrack-Prototype
+//
+//  Created by Clay Suttner on 6/3/21.
+//
+
+import UIKit
+
+class NewDefectViewController: UIViewController {
+    
+}
